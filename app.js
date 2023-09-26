45c20b0125c42c172f0a89dc9cf5fd8f4b02354d
Ainy Afzal <ainyafzaldev@gmail.com>
2023-09-25 21:43:03 -0400

    inital about page

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
 
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs')
+
+app.use(
+  "/css",
+  express.static(path.join(__dirname, "views", "css"))
+)
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "views", "js"))
+)
+
 // Add your server routes and middleware here
 // For example:
- app.get('/', (req, res) => {
+app.get('/', (req, res) => {
+  res.render('pages/index')
+ });
+ app.get('/home', (req, res) => {
   res.render('pages/index')
  });
 
  app.get('/about', (req, res) => {
   res.render('pages/about')
 })
-
+// app.get("*", (req, res) => {
+//   res.render('pages/index')
+//  });
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
